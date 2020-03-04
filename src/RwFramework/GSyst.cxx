34a6e939037efefa0180d3cbe15cf7a5b1fe01a6
Steven Gardiner <gardiner@fnal.gov>
2020-03-03 22:24:05 -0600

    Add the CoulombCCQE knob that scales the value of the EM potential used for Coulomb corrections in the Nieves CCQE model

--- src/RwFramework/GSyst.cxx
+++ src/RwFramework/GSyst.cxx
@@ -121,6 +121,7 @@ std::map<GSyst_t, std::string> GSyst::BuildGSystToStringMap() {
   temp_map[ kXSecTwkDial_NormCCCOH ] = "NormCCCOH";
   temp_map[ kXSecTwkDial_NormNCCOH ] = "NormNCCOH";
   temp_map[ kRDcyTwkDial_Theta_Delta2NRad ] = "ThetaDelta2NRad";
+  temp_map[ kXSecTwkDial_CoulombCCQE ] = "CoulombCCQE";
 
   return temp_map;
 }
